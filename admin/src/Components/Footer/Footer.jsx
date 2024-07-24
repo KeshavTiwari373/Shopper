fc3f91be5b1f394f07fb5891e91e90dd9d8f0268
Keshav <tiwarikeshav373@gmail.com>
2024-07-24 21:40:59 +0530

    Initial commit

---
+++ admin/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr />
+      <p>Copyright @ 2024 - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
